da64fe9ee7ba5dfc0c831d1340e5fee6f95fdbf5
erccool <ecurugan12@gmail.com>
2019-02-18 07:07:15 +0800

    Up this bitch

--- src/com/storeme/javabean/AtmInfoBean.java
+++ src/com/storeme/javabean/AtmInfoBean.java
@@ -1,5 +1,88 @@
 package com.storeme.javabean;
 
 public class AtmInfoBean {
+	public static final String TABLE_NAME = "atminfo";
+	public static final String IDATMINFO = "idatminfo";
+	public static final String ATMPLACEMENT = "atmplacement";
+	public static final String SKU = "sku";
+	public static final String CONSIGNEE = "consignee";
+	public static final String DATE = "date";
+	public static final String TIME = "time";
+	public static final String WAYBILL_NO = "waybill_no";
+	public static final String ACTIVITY = "activtiy";
+	public static final String STATUS = "status";
+	public static final String IDATM = "idatm";
 	
+	public int getIdatminfo() {
+		return idatminfo;
+	}
+	public void setIdatminfo(int idatminfo) {
+		this.idatminfo = idatminfo;
+	}
+	public String getAtmplacement() {
+		return atmplacement;
+	}
+	public void setAtmplacement(String atmplacement) {
+		this.atmplacement = atmplacement;
+	}
+	public String getSku() {
+		return sku;
+	}
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+	public String getConsignee() {
+		return Consignee;
+	}
+	public void setConsignee(String consignee) {
+		Consignee = consignee;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getWaybill_no() {
+		return waybill_no;
+	}
+	public void setWaybill_no(String waybill_no) {
+		this.waybill_no = waybill_no;
+	}
+	public String getActivtiy() {
+		return activtiy;
+	}
+	public void setActivtiy(String activtiy) {
+		this.activtiy = activtiy;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getIdatm() {
+		return idatm;
+	}
+	public void setIdatm(int idatm) {
+		this.idatm = idatm;
+	}
+	private int idatminfo;
+	private String atmplacement;
+	private String sku;
+	private String Consignee;
+	private String date;
+	private String time;
+	private String waybill_no;
+	private String activtiy;
+	private String status;
+	private int idatm;
+	
+
 }
---
+++ src/com/storeme/services/AtmInfoService.java
@@ -0,0 +1,52 @@
+package com.storeme.services;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import com.storeme.javabean.AtmInfoBean;
+
+public class AtmInfoService {
+
+	static String url =("jdbc:mysql://localhost:3306/storeme");
+	static String username ="root";
+	static String password = "password";
+	
+	public ArrayList getAllAtmInfo() {
+		ArrayList<AtmInfoBean> atminfoLists = new ArrayList();
+		
+		String sql1 = "select * from atmninfo";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url,username,password);
+			PreparedStatement st = con.prepareStatement(sql1);
+			ResultSet rs = st.executeQuery();
+			while(rs.next()) {
+				AtmInfoBean ai = new AtmInfoBean();
+				
+				ai.setActivtiy(rs.getString(AtmInfoBean.ACTIVITY));
+				ai.setAtmplacement(rs.getString(AtmInfoBean.ATMPLACEMENT));
+				ai.setConsignee(rs.getString(AtmInfoBean.CONSIGNEE));
+				ai.setDate(rs.getString(AtmInfoBean.DATE));
+				ai.setIdatm(rs.getInt(AtmInfoBean.IDATM));
+				ai.setSku(rs.getString(AtmInfoBean.SKU));
+				ai.setStatus(rs.getString(AtmInfoBean.STATUS));
+				ai.setTime(rs.getString(AtmInfoBean.TIME));
+				ai.setWaybill_no(rs.getString(AtmInfoBean.WAYBILL_NO));
+				
+				atminfoLists.add(ai);
+			}
+			
+			
+		}catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return atminfoLists;
+	}
+	
+	
+}
