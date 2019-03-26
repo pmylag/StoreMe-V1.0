1f95bbf76008b2f49049b2360be02cf5482b9d0c
erccool <ecurugan12@gmail.com>
2019-03-26 10:19:41 +0800

    Update AtmInfoBean.java

--- src/com/storeme/javabean/AtmInfoBean.java
+++ src/com/storeme/javabean/AtmInfoBean.java
@@ -29,6 +29,13 @@ public class AtmInfoBean {
 	
 	private String note;
 	
+	public String getNote(){
+		return note;
+	}
+	
+	public void setNote(String note){
+		this.note = note;
+	}
 	public int getIdatminfo() {
 		return idatminfo;
 	}
